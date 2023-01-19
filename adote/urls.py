0f782430d396efaec3798cdc66c42e325bb1b9e3
ilton <xlsonplay@gmail.com>
2023-01-18 23:30:19 -0300

    Adicionar imagem

--- adote/urls.py
+++ adote/urls.py
@@ -17,10 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
     path('divulgar/', include('divulgar.urls')),
     path('adotar/', include('adotar.urls')),
+    path('', RedirectView.as_view(url='/auth/login/')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
